7d6aeefaba8af845bbd9d029715d313d16fa0b7c
saritchi <s.ritchie1@hotmail.com>
2021-06-19 12:41:01 -0700

    import topics selector, and assigned selector

--- src/components/NewQuizForm.js
+++ src/components/NewQuizForm.js
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import ROUTES from "../app/routes";
+import { selectTopics } from '../features/topics/TopicsSlice'
+import { useSelector } from 'react-redux'
 
 export default function NewQuizForm() {
   const [name, setName] = useState("");
   const [cards, setCards] = useState([]);
   const [topicId, setTopicId] = useState("");
   const history = useHistory();
-  const topics = {};
+  const topics = useSelector(selectTopics);
 
   const handleSubmit = (e) => {
     e.preventDefault();
